a0fad2497dd1e00ef2549014f492d218715b49c4
Gilbert Larochelle Martin <gilbert.larochelle-martin.1@ens.etsmtl.ca>
2022-12-20 16:18:38 -0500

    Added Other Equipment X2 with sensible and latent load

--- main.py
+++ main.py
@@ -1,66 +1,45 @@
-import math, sys
+import sys
+
 sys.path.append("C:\EnergyPlusV22-1-0")
 from pyenergyplus.plugin import EnergyPlusPlugin
-import pandas as pd
-import numpy as np
-
-
-
-
 
 class type205(EnergyPlusPlugin):
 
-    ShadeStatusInteriorBlindOn = 6
-    ShadeStatusOff = 0
-
     def __init__(self):
         super().__init__()
-        self.handles_set = False
-        self.handle_zone_sensible_cooling_rate = None
-        self.handle_solar_beam_incident_cosine = None
-        self.handle_shading_deploy_status = None
-        self.handle_global_shading_actuator_status = None  # only because actuators can be output vars ... yet
-        self.handle_time_of_day = None
 
-    def on_begin_timestep_before_predictor(self, state) -> int:
+        self.handles_set = False
+
+        self.handles_zone_temperature = None
+        self.handles_zone_humidity = None
+
+        ### Outputs back to E+ ###
+        self.handle_zone_sensible_rate = None
+        self.handle_zone_latent_rate = None
+
+        # ### Reporting ###
+        # self.handle_zone_vegetation_temperature = None
+        # self.handle_zone_reflected_radiation = None
+
+    def on_inside_hvac_system_iteration_loop(self, state) -> int:
 
         if not self.handles_set:
-            self.handle_zone_sensible_cooling_rate = self.api.exchange.get_variable_handle(
-                state, "Zone Air System Sensible Cooling Rate", "West Zone"
-            )
-            self.handle_solar_beam_incident_cosine = self.api.exchange.get_variable_handle(
-                state, "Surface Outside Face Beam Solar Incident Angle Cosine Value", "Zn001:Wall001:Win001"
-            )
-            self.handle_shading_deploy_status = self.api.exchange.get_actuator_handle(
-                state, "Window Shading Control", "Control Status", "Zn001:Wall001:Win001"
-            )
-            self.handle_global_shading_actuator_status = self.api.exchange.get_global_handle(
-                state, "Zn001_Wall001_Win001_Shading_Deploy_Status"
-            )
+
+            self.handles_zone_temperature = self.api.exchange.get_variable_handle(state, "Zone Air Temperature", "Perimeter_ZN_4")
+            self.handles_zone_humidity = self.api.exchange.get_variable_handle(state, "Zone Air Humidity Ratio", "Perimeter_ZN_4")
+
+            self.handle_zone_sensible_rate = self.api.exchange.get_actuator_handle(state,"OtherEquipment","Power Level","OTHEQ_SENSIBLE")
+            self.handle_zone_latent_rate = self.api.exchange.get_actuator_handle(state,"OtherEquipment","Power Level","OTHEQ_LATENT")
 
             self.handles_set = True
 
-        current_incident_angle = self.api.exchange.get_variable_value(state, self.handle_solar_beam_incident_cosine)
-        current_sensible_cool_rate = self.api.exchange.get_variable_value(state, self.handle_zone_sensible_cooling_rate)
-        incident_angle_rad = math.acos(current_incident_angle)
-        incident_angle_degrees = math.degrees(incident_angle_rad)
-        value_to_assign = self.ShadeStatusOff
+        current_zone_air_temperature = self.api.exchange.get_variable_value(state, self.handles_zone_temperature)
+        current_zone_air_humidity = self.api.exchange.get_variable_value(state, self.handles_zone_humidity)
 
-        # add variable
-        tod = self.handle_time_of_day = self.api.exchange.current_time(state)
 
-        if (incident_angle_degrees < 45 or current_sensible_cool_rate > 20) and tod >= 9 and tod <= 17:
-            value_to_assign = self.ShadeStatusInteriorBlindOn
+        self.api.exchange.set_actuator_value(state, self.handle_zone_sensible_rate, 0)
+        self.api.exchange.set_actuator_value(state, self.handle_zone_latent_rate, 50000)
 
-        if tod == 5.0:
-            # Create 5 random integers in the range of 10 - 25
-            random_data = np.random.randint(10, 25, size=5)
-            # Create Datfarme with single column of random values
-            df = pd.DataFrame(random_data, columns=['RANDOM VALUES'])
-            # Display the Dataframe
-            print(df)
 
-        self.api.exchange.set_actuator_value(state, self.handle_shading_deploy_status, value_to_assign)
-        self.api.exchange.set_global_value(state, self.handle_global_shading_actuator_status, value_to_assign)
 
         return 0
